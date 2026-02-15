d0276759791d5be3abef0c846d26e1618567ad68
Aryan Bhagat <aryanbhagat20@gmail.com>
2026-02-15 23:50:51 +0530

    Fix CORS for production

--- src/main/java/com/elevator/elevator_system/config/WebSocketConfig.java
+++ src/main/java/com/elevator/elevator_system/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOrigins("https://realtime-elevator-system.netlify.app")
                 .withSockJS();
     }
 }
