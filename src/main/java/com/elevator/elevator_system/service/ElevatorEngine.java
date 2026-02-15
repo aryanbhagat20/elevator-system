f94cc0e3389cf255d7e475cfcf913697fb02ba9e
Aryan Bhagat <aryanbhagat20@gmail.com>
2026-02-15 12:20:59 +0530

    Phase 4: Real-time WebSocket broadcasting for elevator status

--- src/main/java/com/elevator/elevator_system/service/ElevatorEngine.java
+++ src/main/java/com/elevator/elevator_system/service/ElevatorEngine.java
@@ -1,19 +1,33 @@
 package com.elevator.elevator_system.service;
 
+import com.elevator.elevator_system.dto.ElevatorStatusDTO;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ElevatorEngine {
 
     private final ElevatorSystemService elevatorSystemService;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    public ElevatorEngine(ElevatorSystemService elevatorSystemService) {
+    public ElevatorEngine(ElevatorSystemService elevatorSystemService,
+                          SimpMessagingTemplate messagingTemplate) {
+
         this.elevatorSystemService = elevatorSystemService;
+        this.messagingTemplate = messagingTemplate;
     }
 
     @Scheduled(fixedRate = 1000)
     public void tick() {
+
         elevatorSystemService.stepSystem();
+
+        List<ElevatorStatusDTO> status =
+                elevatorSystemService.getElevatorStatus();
+
+        messagingTemplate.convertAndSend("/topic/elevators", status);
     }
 }
