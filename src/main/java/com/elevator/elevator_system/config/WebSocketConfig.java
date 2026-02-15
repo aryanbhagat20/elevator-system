13e1f2196b6cdcc975c8f7b1e1ef0356ab036abb
Aryan Bhagat <aryanbhagat20@gmail.com>
2026-02-16 00:21:37 +0530

    Fix: Update CORS configuration for WebSocket connection

--- src/main/java/com/elevator/elevator_system/config/WebSocketConfig.java
+++ src/main/java/com/elevator/elevator_system/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://realtime-elevator-system.netlify.app")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 }
